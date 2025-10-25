0d33b007299e40d386eb966bf0481c1cb17e8c27
Alex Vincent <alex@far2cool.com>
2025-10-25 22:31:22 +0200

    tidy

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  value = azurerm_container_registry.this.id
-}
-output "name" {
-  value = azurerm_container_registry.this.name
-}
-output "location" {
-  value = azurerm_container_registry.this.location
-}
