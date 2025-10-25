1a3226d5ddba6aa08394d782d9510ed22a21a61f
Alex Vincent <alex@far2cool.com>
2025-10-25 22:16:55 +0200

    initial commit

---
+++ src/outputs.tf
@@ -0,0 +1,9 @@
+output "id" {
+  value = azurerm_container_registry.this.id
+}
+output "name" {
+  value = azurerm_container_registry.this.name
+}
+output "location" {
+  value = azurerm_container_registry.this.location
+}
